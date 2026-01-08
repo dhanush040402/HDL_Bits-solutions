f0a27b9eceb6007b813e16f56fa632577dc0450a
dhanush040402 <dhanushvelmurugan1@gmail.com>
2026-01-08 10:37:12 +0530

    Create 07.Wire decl.v

---
+++ 02.Verilog language/01.Basics/07.Wire decl.v
@@ -0,0 +1,17 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   ); 
+    wire g1;
+    wire g2;
+    wire g3;
+    and(g1,a,b);
+    and(g2,c,d);
+    or(g3,g1,g2);
+    assign out=g3;
+    assign out_n=~g3;
+
+endmodule
