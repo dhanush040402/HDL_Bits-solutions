7bbdb993a04a46e0cf9d53a4cd19f96ca8e3af97
dhanush040402 <dhanushvelmurugan1@gmail.com>
2026-01-15 23:09:29 +0530

    Create 07.module_fadd.v

---
+++ 02.Verilog language/03.modules/07.module_fadd.v
@@ -0,0 +1,15 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    wire w1,w2,w3;
+    add16 a1(.a(a[15:0]),.b(b[15:0]),.cin(w1),.sum(sum[15:0]),.cout(w2));
+    add16 a2(.a(a[31:16]),.b(b[31:16]),.cin(w2),.sum(sum[31:16]),.cout(w3));
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+assign sum=a^b^cin;
+    assign cout=((a&b)|(b&cin)|(a&cin));
+
+endmodule
