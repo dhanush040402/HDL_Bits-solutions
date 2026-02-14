e65221215ae013ea3a1aa118623265509061aa6e
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-14 15:31:04 +0530

    Create 02.Dff8.v

---
+++ 02.Verilog language/06.Circuits/02.Sequential Circuit/01.Latches and Flipflop/02.Dff8.v
@@ -0,0 +1,10 @@
+module top_module (
+    input clk,
+    input [7:0] d,
+    output [7:0] q
+);
+    integer i;
+    always @ (posedge clk)begin
+        for(i=0;i<8;i++)q[i]<=d[i];
+    end
+endmodule
