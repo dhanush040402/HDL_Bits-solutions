7f017dae6f8ebac884a66331ebe8ae0d4f0a5ee4
Dhanush V V <dhanushvelmurugan1@gmail.com>
2026-02-20 18:05:29 +0530

    Create 12.muxxdff.v

---
+++ 02.Verilog language/06.Circuits/02.Sequential Circuit/01.Latches and Flipflop/12.muxxdff.v
@@ -0,0 +1,13 @@
+module top_module (
+    input clk,
+    input w, R, E, L,
+    output Q
+);
+    wire w1,d;
+    assign w1= E?w:Q;
+    assign d=L?R:w1;
+    always @(posedge clk)
+        Q<=d;
+    
+
+endmodule
